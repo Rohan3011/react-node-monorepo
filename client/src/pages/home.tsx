1a5739ab1e3718d4bbfabf7514a11bce9284d2f5
rohanly <rohan.k@ly.design>
2023-12-16 13:14:16 +0530

    Added tailwindcss

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const HomePage: React.FC = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <h1 className="font-bold text-4xl">Home Page</h1>
+    </div>
+  );
 };
 
 export default HomePage;
