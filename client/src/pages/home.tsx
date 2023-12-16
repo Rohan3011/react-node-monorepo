06919aac55312fe428eae4634fb3bc12374f818c
rohanly <rohan.k@ly.design>
2023-12-16 13:58:34 +0530

    Added recoil example

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { counterState, squaredState } from "@/atoms/example";
 
 const HomePage: React.FC = () => {
+  const [counter, setCounter] = useRecoilState(counterState);
+  const squared = useRecoilValue(squaredState);
+
   return (
-    <div className="w-full min-h-screen flex justify-center items-center">
+    <div className="w-full min-h-screen flex flex-col justify-center items-center">
       <h1 className="font-bold text-4xl">Home Page</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+      <span>Count : {counter}</span>
+      <span> Squared : {squared}</span>
+      <button onClick={() => setCounter((prev) => prev - 1)}>-</button>
     </div>
   );
 };
