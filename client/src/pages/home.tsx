5a14855a4b54d6c5c52ef55d80ea29e2c6e581bc
rohanop <rohankamble3011@gmail.com>
2024-01-28 20:45:29 +0530

    Added shadcn ui

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -2,26 +2,27 @@ import React, { useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { counterState, squaredState } from "@/atoms/example";
 import axios from "@/lib/axios";
+import { Button } from "@/components/ui/button";
 
 const HomePage: React.FC = () => {
   const [counter, setCounter] = useRecoilState(counterState);
   const squared = useRecoilValue(squaredState);
 
-  useEffect(() => {
-    const checkHealth = async () => {
-      const { data } = await axios.get("/api/health");
-      console.log(data);
-    };
-    checkHealth();
-  }, []);
+  // useEffect(() => {
+  //   const checkHealth = async () => {
+  //     const { data } = await axios.get("/api/health");
+  //     console.log(data);
+  //   };
+  //   checkHealth();
+  // }, []);
 
   return (
     <div className="w-full min-h-screen flex flex-col justify-center items-center">
       <h1 className="font-bold text-4xl">Home Page</h1>
-      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+      <Button onClick={() => setCounter((prev) => prev + 1)}>+</Button>
       <span>Count : {counter}</span>
       <span> Squared : {squared}</span>
-      <button onClick={() => setCounter((prev) => prev - 1)}>-</button>
+      <Button onClick={() => setCounter((prev) => prev - 1)}>-</Button>
     </div>
   );
 };
